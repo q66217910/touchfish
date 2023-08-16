1f3bce5b3a9fb24aa93f4776869a75c307fcd4dc
zhangde <782809011@qq.com>
2023-08-17 03:16:08 +0800

    灾厄之林

--- _posts/game/lua/测试位置脚本.lua
+++ _posts/game/lua/测试位置脚本.lua
@@ -1,6 +1,21 @@
 function OnEvent(event, arg)
-      if(event == 'MOUSE_BUTTON_RELEASED' and arg==4 ) then
+      if(event == 'MOUSE_BUTTON_RELEASED' and arg==5 ) then
             x, y = GetMousePosition();
             OutputLogMessage("Mouse is at %d, %d\n", x, y);
      end
+     if(event == 'MOUSE_BUTTON_RELEASED' and arg==5 ) then
+        move()
+     end
+end
+
+--[[
+ÒÆ¶¯
+]]
+function move(x,y)
+	MoveMouseTo(x,y)
+    Sleep(500)
+    PressMouseButton(1)
+    Sleep(100)
+    ReleaseMouseButton(1)
+    Sleep(1000)
 end
